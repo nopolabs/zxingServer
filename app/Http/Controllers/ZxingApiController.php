e389f85e7a8b0625c77553d29deb47ce2a47159b
drevel <drevel@opensky.com>
2019-02-03 15:43:11 -0800

    formatting

--- app/Http/Controllers/ZxingApiController.php
+++ app/Http/Controllers/ZxingApiController.php
@@ -16,17 +16,17 @@ class ZxingApiController extends Controller
         $this->scanner = $scanner;
     }
 
-   public function scan(Request $request)
-   {
-       if ($request->exists('image')) {
-           $path = $request->file('image')->store('temp');
-       } else {
-           $path = 'temp/' . uniqid('ZxingApiController_', true);
-           $contents = $request->exists('url')
-               ? file_get_contents($request->get('url'))
-               : $request->getContent(true);
-           Storage::disk()->put($path, $contents);
-       }
+    public function scan(Request $request)
+    {
+        if ($request->exists('image')) {
+            $path = $request->file('image')->store('temp');
+        } else {
+            $path = 'temp/' . uniqid('ZxingApiController_', true);
+            $contents = $request->exists('url')
+                ? file_get_contents($request->get('url'))
+                : $request->getContent(true);
+            Storage::disk()->put($path, $contents);
+        }
 
         $storagePath = Storage::disk()->getDriver()->getAdapter()->getPathPrefix();
         $fullPath = $storagePath.$path;
